1d22d374310324956ed8694ddd78c795baeb2900
Yogiraj Phalke <155828066+YOGIRAJPHALKE@users.noreply.github.com>
2026-05-12 23:20:11 +0530

    .
    .

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+using StarterAssets;
+
+public class Robot : MonoBehaviour
+{
+    //[SerializeField] Transform target;
+    FirstPersonController player;
+
+    NavMeshAgent agent;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Start()
+    {
+        player = FindFirstObjectByType<FirstPersonController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
