99b4d807e79ac8139a43f61b1aab5684315b77d9
Yogiraj Phalke <155828066+YOGIRAJPHALKE@users.noreply.github.com>
2026-06-09 00:09:53 +0530

    added shoot cooldown for weapons

---
+++ Assets/Scripts/ActiveWeapon.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using StarterAssets;
+
+public class ActiveWeapon : MonoBehaviour
+{
+    
+    [SerializeField] WeaponSO weaponSO; 
+    Animator animator;
+
+    StarterAssetsInputs starterAssetsInputs;
+    Wepon currentWeapon;
+
+    const string SHOOT_STRING="Shoot";
+
+    float timeSinceLastShoot= 0f;
+
+    void Awake() 
+    {
+        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        animator = GetComponent<Animator>();
+    } 
+    void Start() 
+    {
+        currentWeapon = GetComponentInChildren<Wepon>();  
+    }
+    void Update()
+    {
+        timeSinceLastShoot += Time.deltaTime;
+        HandleShoot();
+    }
+
+    void HandleShoot()
+    {
+        if (!starterAssetsInputs.shoot) return;
+
+        if(timeSinceLastShoot >= weaponSO.FireRate)
+        {
+            currentWeapon.Shoot(weaponSO);
+            animator.Play(SHOOT_STRING, 0, 0f);
+            timeSinceLastShoot = 0f;
+        }
+        starterAssetsInputs.ShootInput(false);
+    }
+}
--- Assets/Scripts/Wepon.cs
+++ Assets/Scripts/Wepon.cs
@@ -1,42 +1,18 @@
 using UnityEngine;
-using Unity.Mathematics;
-using StarterAssets;
+
 
 public class Wepon : MonoBehaviour
 {
-    [SerializeField] WeaponSO weaponSO; 
-    [SerializeField] GameObject hitVFXPrefab;
-    [SerializeField] Animator animator;
     [SerializeField] ParticleSystem muzzelFlash;
-    //[SerializeField] int damageAmount= 1;
 
-    StarterAssetsInputs starterAssetsInputs;
-
-    const string SHOOT_STRING="Shoot";
-    void Awake() 
+    public void Shoot(WeaponSO weaponSO)
     {
-        starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
-
-    } 
-    void Update()
-    {
-        HandleShoot();
-    }
-
-    void HandleShoot()
-    {
-        if (!starterAssetsInputs.shoot) return;
-
         muzzelFlash.Play();
-        animator.Play(SHOOT_STRING, 0, 0f);
-        
-        starterAssetsInputs.ShootInput(false);
-
         RaycastHit hit;
 
         if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-            Instantiate(hitVFXPrefab, hit.point, Quaternion.identity);
+            Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
             EnemyHelth enemyHealth = hit.collider.GetComponent<EnemyHelth>();
             enemyHealth?.TakeDamage(weaponSO.Damage);
 
