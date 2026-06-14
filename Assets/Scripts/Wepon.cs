4465d8881750a5cccc12782991b8e0e93346df15
Yogiraj Phalke <155828066+YOGIRAJPHALKE@users.noreply.github.com>
2026-06-14 17:53:13 +0530

    added ignore Raycasat for pickup

--- Assets/Scripts/Wepon.cs
+++ Assets/Scripts/Wepon.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class Wepon : MonoBehaviour
 {
     [SerializeField] ParticleSystem muzzelFlash;
+    [SerializeField] LayerMask interactionLayers;
 
     public void Shoot(WeaponSO weaponSO)
     {
         muzzelFlash.Play();
         RaycastHit hit;
 
-        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity,interactionLayers,QueryTriggerInteraction.Ignore))
         {
             Instantiate(weaponSO.HitVFXPrefab, hit.point, Quaternion.identity);
             EnemyHelth enemyHealth = hit.collider.GetComponent<EnemyHelth>();
